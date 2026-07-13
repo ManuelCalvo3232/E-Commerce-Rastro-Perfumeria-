f2f8b6121be408b1509609455df099e10647a95b
ManuelCalvo3232 <manuelcalvotec4@gmail.com>
2026-07-13 16:49:14 -0300

    Add product catalog route with sample fragrances

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -32,6 +32,29 @@ $app->get("/", function ($request, $response) use ($renderer) {
   return view($renderer, $response, "index.php");
 });
 
+// Ruta/Productos
+$app->get("/productos", function ($request, $response) use ($renderer) {
+  $catalogo = [
+    "Masculino" => [
+      ["nombre" => "Bleu de Chanel", "marca" => "Chanel", "precio" => 4500, "ml" => 5, "imagen" => "/img/bleu-chanel.jpg"],
+      ["nombre" => "Sauvage", "marca" => "Dior", "precio" => 4200, "ml" => 5, "imagen" => "/img/sauvage.jpg"],
+      ["nombre" => "Aventus", "marca" => "Creed", "precio" => 8900, "ml" => 5, "imagen" => "/img/aventus.jpg"],
+    ],
+    "Femenino" => [
+      ["nombre" => "Good Girl", "marca" => "Carolina Herrera", "precio" => 4300, "ml" => 5, "imagen" => "/img/good-girl.jpg"],
+      ["nombre" => "Black Opium", "marca" => "YSL", "precio" => 4100, "ml" => 5, "imagen" => "/img/black-opium.jpg"],
+    ],
+    "Nicho" => [
+      ["nombre" => "Baccarat Rouge 540", "marca" => "Maison Francis Kurkdjian", "precio" => 9800, "ml" => 5, "imagen" => "/img/baccarat-540.jpg"],
+    ],
+  ];
+
+  return view($renderer, $response, "productos.php", [
+    "title" => "Catálogo | Rastro Perfumería",
+    "catalogo" => $catalogo,
+  ]);
+});
+
 $app->addErrorMiddleware($debug, true, true);
 
 return $app;
