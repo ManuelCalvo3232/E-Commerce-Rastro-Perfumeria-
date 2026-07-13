e4349173a44259af13fc227741251b18aae8f0e8
ManuelCalvo3232 <manuelcalvotec4@gmail.com>
2026-07-13 17:25:41 -0300

    Add missing stylesheet link in base layout

--- src/views/layouts/base.php
+++ src/views/layouts/base.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title><?= html($title) ?></title>
+        <link rel="stylesheet" href="/css/style.css" />
     </head>
     <body>
        <?php require __DIR__ . "/../partials/navbar.php"; ?>
