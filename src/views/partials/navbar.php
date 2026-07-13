b9142aef30ef2fcb0e6e295d46925aa0f65fafd6
ManuelCalvo3232 <manuelcalvotec4@gmail.com>
2026-07-13 17:23:39 -0300

    Add base styles and update navbar structure with logo image

--- src/views/partials/navbar.php
+++ src/views/partials/navbar.php
@@ -1,10 +1,13 @@
 <header class="navbar">
-    <a href="/" class="navbar__logo">Rastro Perfumería</a>
+    <a href="/" class="navbar__logo">
+        <img src="/img/logo-mark.svg" alt="">
+        Rastro Perfumería
+    </a>
     <nav class="navbar__links">
         <a href="/">Inicio</a>
         <a href="/productos">Productos</a>
         <a href="/decants">Decants</a>
         <a href="/contacto">Contacto</a>
     </nav>
-    <div class="navbar__cart">🛒 Carrito</div>
+    <div class="navbar__cart">Carrito</div>
 </header>
