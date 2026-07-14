34429aa092f90c0de32ce2908178d96f65250f2c
ManuelCalvo3232 <manuelcalvotec4@gmail.com>
2026-07-13 23:43:26 -0300

    Refactor product catalog structure in bootstrap.php for improved clarity and consistency

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -29,33 +29,16 @@ $renderer = new PhpRenderer(
 
 // Ruta/Vista principal
 $app->get("/", function ($request, $response) use ($renderer) {
-  return view($renderer, $response, "index.php");
-});
-
-// Ruta/Productos
-$app->get("/productos", function ($request, $response) use ($renderer) {
-  $catalogo = [
-    "Masculino" => [
-      ["nombre" => "Bleu de Chanel", "marca" => "Chanel", "precio100ml" => 100000, "precio10ml" => 10000, "precio5ml" => 4500, "10ml" => 10, "5ml" => 5, "imagen" => "/img/bleu-chanel.jpg"],
-      ["nombre" => "Sauvage", "marca" => "Dior", "precio100ml" => 4200, "precio10ml" => 420, "precio5ml" => 210, "10ml" => 10, "5ml" => 5, "imagen" => "/img/sauvage.jpg"],
-      ["nombre" => "Aventus", "marca" => "Creed", "precio100ml" => 8900, "precio10ml" => 890, "precio5ml" => 445, "10ml" => 10, "5ml" => 5, "imagen" => "/img/aventus.jpg"],
-    ],
-    "Femenino" => [
-      ["nombre" => "Good Girl", "marca" => "Carolina Herrera", "precio100ml" => 4300, "precio10ml" => 430, "precio5ml" => 215, "10ml" => 10, "5ml" => 5, "imagen" => "/img/good-girl.jpg"],
-      ["nombre" => "Black Opium", "marca" => "YSL", "precio100ml" => 4100, "precio10ml" => 410, "precio5ml" => 205, "10ml" => 10, "5ml" => 5, "imagen" => "/img/black-opium.jpg"],
-    ],
-    "Nicho" => [
-      ["nombre" => "Baccarat Rouge 540", "marca" => "Maison Francis Kurkdjian", "precio100ml" => 9800, "precio10ml" => 980, "precio5ml" => 490, "10ml" => 10, "5ml" => 5, "imagen" => "/img/baccarat-540.jpg"],
-    ],
+  $productos = [
+    ["nombre" => "Bleu de Chanel", "marca" => "Chanel", "precio" => 4500, "ml" => 5, "imagen" => "/img/bleu-chanel.jpg"],
+    ["nombre" => "Sauvage", "marca" => "Dior", "precio" => 4200, "ml" => 5, "imagen" => "/img/sauvage.jpg"],
+    ["nombre" => "Aventus", "marca" => "Creed", "precio" => 8900, "ml" => 5, "imagen" => "/img/aventus.jpg"],
   ];
 
-  return view($renderer, $response, "productos.php", [
-    "title" => "Catálogo | Rastro Perfumería",
-    "catalogo" => $catalogo,
-  ]);
+  return view($renderer, $response, "index.php", ["productos" => $productos]);
 });
 
-// . 
+// Ruta/Productos (listado)
 $app->get("/productos", function ($request, $response) use ($renderer) {
   return view($renderer, $response, "productos/index.php", [
     "title" => "Listado de Productos",
