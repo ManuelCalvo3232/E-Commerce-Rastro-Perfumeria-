0f44d4d10bee4faa369ba458e193a280b46debbf
ManuelCalvo3232 <manuelcalvotec4@gmail.com>
2026-07-13 21:21:02 -0300

    Create error.php

---
+++ src/views/productos/error.php
@@ -0,0 +1,5 @@
+<main class="form-container">
+    <h1>Error al crear producto</h1>
+    <p>Faltan datos obligatorios. Volvé al formulario y completá todos los campos.</p>
+    <a href="/create/productos">&larr; Volver al formulario</a>
+</main>
